7ec3df74d343ae54cee805105b2d9125d22d21aa
shankarshannu <shankar.europlak>
2021-03-07 21:28:59 +0530

    extra changes for rebase

--- readme.md
+++ readme.md
@@ -6,3 +6,7 @@
 
 ## feature1 change for merging
 ## feature changes for merge in rel_1.0
+
+## this change for rebase in master
+
+## fix the bug for changes
--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
 changes for rel_1.0
 feature1 change for merging
 feature changes for merge in rel_1.0
+this change for rebase in master
+
+fix the bug for changes
